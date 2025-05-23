035d79fd206fc303d7073797ab9a0c25594d345b
Ilyass <ilyassovitche1234@gmail.com>
2025-05-23 03:16:23 +0100

    deploy

--- Backend/server.js
+++ Backend/server.js
@@ -1,41 +1,37 @@
 const express = require("express");
 const cors = require("cors");
 const env = require("dotenv");
-const app = express();
-const ConnectDB = require("./config/db");
-const authRoutes =require("./Routes/authRoutes")
-const clientRoutes =require("./Routes/clientRoutes")
-const technicianRoutes=require("./Routes/technicianRoutes")
-const adminRoutes=require("./Routes/adminRoutes")
-const interventionRoutes=require("./Routes/interventionRoutes")
-const uploadRoutes = require("./Routes/uploadRoutes");
-const supportRoutes=require("./Routes/supportRoutes")
+const serverless = require("serverless-http");
 
-app.use(express.json());
+const ConnectDB = require("./config/db");
+const authRoutes = require("./Routes/authRoutes");
+const clientRoutes = require("./Routes/clientRoutes");
+const technicianRoutes = require("./Routes/technicianRoutes");
+const adminRoutes = require("./Routes/adminRoutes");
+const interventionRoutes = require("./Routes/interventionRoutes");
+const uploadRoutes = require("./Routes/uploadRoutes");
+const supportRoutes = require("./Routes/supportRoutes");
+
 env.config();
 
-const PORT = process.env.PORT || 3000;
+const app = express();
 
-app.use(
-  cors({
-    origin: ["https://opm-omega.vercel.app/", "http://localhost:9000"],
-    credentials: true,
-  })
-);
+app.use(express.json());
+app.use(cors());
+
 ConnectDB();
 
 app.use("/api/auth", authRoutes);
 app.use("/api/client", clientRoutes);
 app.use("/api/technician", technicianRoutes);
-app.use("/api/admin",adminRoutes)
-app.use("/api/interventions",interventionRoutes)
+app.use("/api/admin", adminRoutes);
+app.use("/api/interventions", interventionRoutes);
 app.use("/api/upload", uploadRoutes);
 app.use("/api/support", supportRoutes);
 
-
 app.get("/", (req, res) => {
-  res.send("BACKEND API IS WORKKING!");
-}),
-  app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-  });
+  res.send("BACKEND API IS WORKING!");
+});
+
+// Remove app.listen() — instead export handler:
+module.exports = serverless(app);
